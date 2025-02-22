7b6aa56e9ec0ba99dfd60acba80ed61fefb1ea84
dkslinkyyy <slinky.sthlm@gmail.com>
2025-02-22 20:48:57 +0100

    vite config preview allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  preview: {
+    allowedHosts: ["fr-projekt-production.up.railway.app"],
+  },
   server: {
     host: "0.0.0.0",
     port: Number(process.env.PORT) || 4173,
