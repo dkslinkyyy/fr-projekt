b725f02f17dc673f51a86cfc35e3d81df2985314
dkslinkyyy <slinky.sthlm@gmail.com>
2025-02-22 19:24:54 +0100

    more changes.. built the app

--- src/hooks/useCatFact.tsx
+++ src/hooks/useCatFact.tsx
@@ -1,1 +1,22 @@
+import { useState } from "react";
 
+const useCatFact = () => {
+  const API_URL = "https://catfact.ninja/fact";
+  const [fact, setFact] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const fetchCatFact = async () => {
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) throw new Error("Fejlade att fånga en katt.. error");
+      const data = await response.json();
+      setFact(data.fact);
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  return { fact, error, fetchCatFact };
+};
+
+export default useCatFact;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,23 @@
 import { useNavigate } from "react-router-dom";
-import CustomLink from "../components/CustomLink";
 import Card from "../components/Card";
 import { usePersistedUser } from "../hooks/usePersistedUser";
-import { useEffect } from "react";
-import LogoutButton from "../components/LogoutButton";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import useCatFact from "../hooks/useCatFact";
 
 const Home = () => {
   const { user, loading } = usePersistedUser();
   const navigate = useNavigate();
+  const { fact, error, fetchCatFact } = useCatFact();
+  const [isFetching, setIsFetching] = useState<boolean>(false);
 
+  const handleFetchCatFact = async () => {
+    setIsFetching(true);
+    await fetchCatFact();
+    setIsFetching(false);
+  };
   useEffect(() => {
     if (!loading && !user) {
-
       navigate("/", { replace: true });
     }
   }, [user, loading, navigate]);
@@ -29,9 +34,26 @@ const Home = () => {
     <main>
       <Card title="Huvudsida" logoutBtn={true}>
         <p>Välkommen, {user.username}!</p>
-        <p style={{ color: "lightgreen" }}>
-          {user.isAdmin ? "Du har admin perms.." : ""}
+        <p style={{ color: "lightgreen", fontWeight: "bold" }}>
+          {user.isAdmin ? "Du har fulla rättigheter." : ""}
         </p>
+
+        <div className="fact-section">
+          <p>Random fakta om katter:</p>
+          <button
+            style={{ width: "50%" }}
+            onClick={handleFetchCatFact}
+            disabled={isFetching}
+          >
+            {isFetching ? "Hämtar..." : "Få Random Fakta"}
+          </button>{" "}
+          {error ? (
+            <p style={{ color: "red" }}>Error: {error}</p>
+          ) : (
+            <p style={{ color: "lightgray", fontStyle: "italic" }}>{fact}</p>
+          )}
+        </div>
+
         <Link className="custom-link bottom" to="/about">
           Om applikationen
         </Link>
